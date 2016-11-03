131384172f6e00fe65153ec6e615de5af3fa9371
Riza Fahmi <rizafahmi@gmail.com>
2016-11-03 10:50:45 +0700

    Create `uptime` command and send it with DM

--- index.js
+++ index.js
@@ -15,44 +15,75 @@ let slack = new RtmClient(token, {
   autoMark: true
 })
 
-slack.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
-  let user = slack.dataStore.getUserById(slack.activeUserId)
-  let team = slack.dataStore.getTeamById(slack.activeTeamId)
+// slack.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+//   let user = slack.dataStore.getUserById(slack.activeUserId)
+//   let team = slack.dataStore.getTeamById(slack.activeTeamId)
 
-  console.log(`Connected to ${team.name} as ${user.name}`)
+//   console.log(`Connected to ${team.name} as ${user.name}`)
 
-  let groups = helpers.getGroups(slack.dataStore.groups)
-  let groupNames = groups.map((group) => {
-    return group.name
-  }).join(', ')
+//   let groups = helpers.getGroups(slack.dataStore.groups)
+//   let groupNames = groups.map((group) => {
+//     return group.name
+//   }).join(', ')
 
-  let channels = helpers.getGroups(slack.dataStore.channels)
-  let channelNames = channels.map((channel) => {
-    return channel.name
-  }).join(', ')
+//   let channels = helpers.getGroups(slack.dataStore.channels)
+//   let channelNames = channels.map((channel) => {
+//     return channel.name
+//   }).join(', ')
 
-  console.log(`Currently in groups: ${groupNames}`)
-  console.log(`Currently in channels: ${channelNames}`)
+//   console.log(`Currently in groups: ${groupNames}`)
+//   console.log(`Currently in channels: ${channelNames}`)
 
-  groups.forEach((group) => {
-    let members = group.members.map((id) => {
-      return slack.dataStore.getUserById(id)
-    })
+//   groups.forEach((group) => {
+//     let members = group.members.map((id) => {
+//       return slack.dataStore.getUserById(id)
+//     })
 
-    members = members.filter((member) => {
-      return !member.is_bot
-    })
+//     members = members.filter((member) => {
+//       return !member.is_bot
+//     })
 
-    let memberNames = members.map((member) => {
-      return member.name
-    }).join(', ')
-    console.log(`Members of '${group.name}' channel: ${memberNames}`)
+//     let memberNames = members.map((member) => {
+//       return member.name
+//     }).join(', ')
+//     console.log(`Members of '${group.name}' channel: ${memberNames}`)
 
-    slack.sendMessage(`Hello ${memberNames}!`, group.id)
+//     slack.sendMessage(`Hello ${memberNames}!`, group.id)
 
-  })
+//   })
 
 
+// })
+
+slack.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`)
+})
+
+slack.on(RTM_EVENTS.MESSAGE, (message) => {
+  let user = slack.dataStore.getUserById(message.user)
+
+  if (user && user.is_bot) {
+    return
+  }
+
+  let channel = slack.dataStore.getChannelGroupOrDMById(message.channel)
+
+  if (message.text) {
+    let msg = message.text.toLowerCase()
+
+    if (/(hello|hi) (bot|awesomebot)/g.test(msg)) {
+      slack.sendMessage(`Hello to you too, ${user.name}!`, channel.id)
+    }
+    if(/uptime/g.test(msg)) {
+      let dm = slack.dataStore.getDMByName(user.name)
+      let uptime = process.uptime()
+      let minutes = parseInt(uptime / 60, 10)
+      let hours = parseInt(minutes / 60, 10)
+      let seconds = parseInt(uptime - (minutes * 60) - ((hours * 60) * 60), 10)
+
+      slack.sendMessage(`I have been running for: ${hours} hours, ${minutes} minutes and ${seconds} seconds.`, dm.id)
+    }
+  }
 })
 
 slack.start()
